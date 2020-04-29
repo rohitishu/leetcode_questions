766a99537791e6d1d35d9e2f3ca8efc0bfac264c
Rohit Kumar <32030787+rohitishu@users.noreply.github.com>
2020-04-29 14:05:06 +0530

    roating the substring left and right

---
+++ April_challenge/perform_string_shift.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string stringShift(string s, vector<vector<int>>& shift) {
+        
+        int n=s.size();
+        for(int i=0;i<shift.size();i++)
+        {   string s1=s,s2=s;
+            int d=shift[i][1];
+            if(shift[i][0]==0)
+            {
+                
+                s=s1.erase(0,d)+s2.erase(d,n);
+            }
+            else
+            {
+              s=s1.erase(0,n-d)+s2.erase(n-d,n);
+            }
+        }
+        return s;
+    }
+};
